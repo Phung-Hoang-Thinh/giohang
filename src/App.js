9e03652c12a4c96f43f2d406add1ed4ff6d02d8d
Phung-Hoang-Thinh <122436388+Phung-Hoang-Thinh@users.noreply.github.com>
2024-01-05 18:22:46 +0700

    code gio hang

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import GioHang from './Components/GioHang'
+
+function App() {
+  return (
+    <div>
+      <GioHang/>
+
+    </div>
+    
+  );
+}
+
+export default App;
